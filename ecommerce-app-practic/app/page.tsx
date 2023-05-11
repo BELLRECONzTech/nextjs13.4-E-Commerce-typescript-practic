e9158b164f7e4ea1150f64a9591c5804914ccffa
BELLRECONzTech <122868815+BELLRECONzTech@users.noreply.github.com>
2023-05-12 02:25:32 +0700

    Product typescipt

---
+++ ecommerce-app-practic/app/page.tsx
@@ -0,0 +1,20 @@
+import Product from "@/components/Product";
+
+export default async function Home() {
+  
+  const res = await fetch('https://fakestoreapi.com/products');
+  const products: Product[] = await res.json();
+
+  return (
+    <main className="">
+      <section className="flex flex-col space-y-12 pb-44">
+        <h1 className="text-5xl font-bold text-center">DEALS OF THE DAY</h1>
+        
+
+        {products.map((product) =>(
+          <Product key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
+  )
+}
